843cf9eb087f6e2f6c12296c8d85a94749543c70
Andrey <andrpts0@gmail.com>
2026-02-04 22:55:02 +0300

    fix3.0

--- pentolymp/settings.py
+++ pentolymp/settings.py
@@ -145,10 +145,6 @@ SIMPLE_JWT = {
 AUTH_USER_MODEL = 'users.User'
 
 DATABASES = {
-    # "default": {
-    #     "ENGINE": "django.db.backends.sqlite3",
-    #     "NAME": BASE_DIR / "db.sqlite3",
-    # }
 'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'db',
